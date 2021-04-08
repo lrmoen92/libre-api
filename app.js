61a9af0330e4fc7392f769b86a042ef7607f381e
BuildTools <unconfigured@null.spigotmc.org>
2021-04-07 20:38:54 -0500

    updated database ip

--- app.js
+++ app.js
@@ -3,43 +3,39 @@ const Sequelize = require('sequelize');
 const app = express()
 const port = 3000
 
-// const sequelize = new Sequelize('mssql://SA:OVER_the_RAINBOW!@192.168.0.53:1433/libre_sql_dev');
-
-
-const sequelize = new Sequelize(
-    'libre_sql_dev', 'SA', 'OVER_the_RAINBOW!', {
-        host: '192.168.0.53',
-        dialect: 'mssql',
-        schema: 'auth',
-        define: {
-            freezeTableName: true
-          }
-    });
+const sequelize = new Sequelize('libre_sql_dev', 'SA', 'OVER_the_RAINBOW!', {
+    host: '192.168.0.66',
+    dialect: 'mssql',
+    schema: 'auth',
+    define: {
+        freezeTableName: true
+    }
+});
 
 sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
 
 const User = sequelize.define('user', { name: Sequelize.STRING });
 
 sequelize.sync({ force: true })
-  .then(() => {
+    .then(() => {
     console.log(`Database & tables created!`);
     User.bulkCreate([
         { name: 'Logan' },
         { name: 'Addison' },
         { name: 'Ace' },
-      ]).then(function() {
+        ]).then(function() {
         return User.findAll();
-      }).then(function(notes) {
+        }).then(function(notes) {
         console.log(notes);
-      });
-  });
+        });
+    });
 
 app.get('/:userId', async (req, res) => {
     console.log(req.params);
@@ -49,11 +45,9 @@ app.get('/:userId', async (req, res) => {
             id: id
         }
     })
-    
-
-  res.send(`Hello ${users[0].name}`);
+    res.send(`Hello ${users[0].name}`);
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
